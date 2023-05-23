c47a142ff3f8dec5544b5d1cabfe4d0502a82645
nicolas <nicolasvazquezzzz@gmail.com>
2023-05-23 12:31:55 -0300

    mejorar de rendimiento

--- js/templatemo-script.js
+++ js/templatemo-script.js
@@ -106,3 +106,57 @@ targets.forEach(target => {
 		t.classList.add('active')
 	})
 })
+
+// Initialize EmailJS with your user ID
+emailjs.init("YOUR_USER_ID");
+
+// Function to send the email
+function sendEmail(event) {
+  event.preventDefault(); // Prevent the form from submitting normally
+
+  // Get the form input values
+  var name = document.getElementById("name-input").value;
+  var email = document.getElementById("email-input").value;
+  var message = document.getElementById("message-input").value;
+
+  // Prepare the email parameters
+  var templateParams = {
+    from_name: name,
+    from_email: email,
+    message: message
+  };
+}
+/*
+  // Initialize EmailJS with your user ID
+emailjs.init("YOUR_USER_ID");
+
+// Function to send the email
+function sendEmail(event) {
+  event.preventDefault(); // Prevent the form from submitting normally
+
+  // Get the form input values
+  var name = document.getElementById("name-input").value;
+  var email = document.getElementById("email-input").value;
+  var message = document.getElementById("message-input").value;
+
+  // Prepare the email parameters
+  var templateParams = {
+    from_name: name,
+    from_email: email,
+    message: message
+  };
+
+  // Send the email using EmailJS
+  emailjs.send("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", templateParams)
+    .then(function(response) {
+      console.log("Email sent successfully:", response.status, response.text);
+      alert("Email sent successfully!");
+    }, function(error) {
+      console.log("Error sending email:", error);
+      alert("An error occurred while sending the email.");
+    });
+}
+
+// Add event listener to the form submission
+document.getElementById("contact-form").addEventListener("submit", sendEmail);
+*/
